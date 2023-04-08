a0df2ee3dd004e13255096e99b9b0356140e2181
Sam Blundell <samcalblundell@googlemail.com>
2023-04-08 16:47:08 +0100

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, () => {});
+
+client.login(process.env.BOT_TOKEN);
